4ab5c29a7b9548917d3278a41f6150a06e3ea907
Mendes <flaviojmendes@gmail.com>
2022-02-13 22:56:22 +0000

    Adding twitter and github checking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 from service.streamer_service import get_streamers
 
@@ -21,5 +22,15 @@ app.add_middleware(
 
 @app.get("/")
 async def root():  
-
     return get_streamers()
+
+
+    
+if __name__ == '__main__':
+    uvicorn.run("app.main:app",
+                host="0.0.0.0",
+                port=8000,
+                reload=True,
+                ssl_keyfile="/etc/letsencrypt/live/brstreamers.dev/privkey.pem", 
+                ssl_certfile="/etc/letsencrypt/live/brstreamers.dev/cert.pem"
+                )
