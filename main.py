378dbd478dcf7d0d04eacbfa9971ef439acd3712
Mendes <flaviojmendes@gmail.com>
2022-02-13 23:00:24 +0000

    Adding twitter and github checking

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def root():
 
     
 if __name__ == '__main__':
-    uvicorn.run("app.main:app",
+    uvicorn.run("main:app",
                 host="0.0.0.0",
                 port=8000,
                 reload=True,
