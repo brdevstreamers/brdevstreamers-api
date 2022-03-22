73ed19215ec6b284b209a0b00ae8adedfea6ee1c
Mendes <flaviojmendes@gmail.com>
2022-03-22 16:58:38 +0000

    returning contributors

--- service/github_service.py
+++ service/github_service.py
@@ -10,12 +10,15 @@ def get_contributors():
     r_ui = requests.get('https://api.github.com/repos/Br-Dev-Streamers/brdevstreamers-ui/contributors')
     contrib_ui = r_ui.json()
 
-    contributors_dict = {}
-    
+    contributors_list = []
+
     for c in contrib_server:
-        contributors_dict[c['login']] = c["avatar_url"]
+        contributor = {"name": c['login'], "image": c["avatar_url"]}
+        contributors_list.append(contributor)
     
     for c in contrib_ui:
-        contributors_dict[c['login']] = c["avatar_url"]
-    return contributors_dict.items()
+        contributor = {"name": c['login'], "image": c["avatar_url"]}
+        contributors_list.append(contributor)
+
+    return contributors_list
 
