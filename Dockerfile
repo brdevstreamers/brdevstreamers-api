cbd96cdcc54a7fcdea5476c0795db88ad3d575c9
Flavio Mendes <flaviojmendes@gmail.com>
2022-07-14 11:16:40 +0100

    merge

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /code
 
 COPY ./requirements.txt /code/requirements.txt
 
+COPY /etc/letsencrypt/live/api.brstreamers.dev/privkey.pem /code/privkey.pem
+COPY /etc/letsencrypt/live/api.brstreamers.dev/cert.pem /code/cert.pem
+
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY ./ /code/
--- main.py
+++ main.py
@@ -38,10 +38,10 @@ if __name__ == '__main__':
 
         uvicorn.run("main:app",
                     host="0.0.0.0",
-                    port=8000,
+                    port=80,
                     reload=True,
-                    ssl_keyfile="/etc/letsencrypt/privkey.pem",
-                    ssl_certfile="/etc/letsencrypt/cert.pem"
+                    ssl_keyfile="privkey.pem",
+                    ssl_certfile="cert.pem"
                     )
     else:
         uvicorn.run("main:app",
