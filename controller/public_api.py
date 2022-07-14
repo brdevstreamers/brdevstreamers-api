d052a3759efe5c4780d41e98fe48ac1cc059a279
Flavio Mendes <flaviojmendes@gmail.com>
2022-07-14 10:12:53 +0100

    no db dep

--- controller/public_api.py
+++ controller/public_api.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from service.github_service import get_contributors
 
-from service.stats_service import compute_stat, get_stats, get_stats_summary
+# from service.stats_service import compute_stat, get_stats, get_stats_summary
 from service.twitch_service import TwitchService
 from view_model.stats_viewmodel import StatsViewModel
 from view_model.stream_viewmodel import StreamViewModel
@@ -39,9 +39,9 @@ async def vods():
     return twitch_service.get_vods()
 
 
-@app_public.get("/stats", response_model=List[StatsViewModel])
-async def stats():
-    return get_stats()
+# @app_public.get("/stats", response_model=List[StatsViewModel])
+# async def stats():
+#     return get_stats()
 
 
 @app_public.get("/tags", response_model=List[TagViewModel])
@@ -50,9 +50,9 @@ async def tags():
     return twitch_service.get_tags()
 
 
-@app_public.get("/stats/summary")
-async def stats_summary():
-    return get_stats_summary()
+# @app_public.get("/stats/summary")
+# async def stats_summary():
+#     return get_stats_summary()
 
 
 @app_public.get("/contributors")
