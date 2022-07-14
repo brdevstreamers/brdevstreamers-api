a1a41d55c80791e3f9b0dd5a5ef92c0adca1912f
Flavio Mendes <flaviojmendes@gmail.com>
2022-07-14 11:28:57 +0100

    merge

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
         uvicorn.run("main:app",
                     host="0.0.0.0",
-                    port=80,
+                    port=8000,
                     reload=True,
                     ssl_keyfile="privkey.pem",
                     ssl_certfile="cert.pem"
